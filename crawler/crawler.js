01c2891c9aa733294bcaea929980d20a4ae46e1c
neal1991 <neal1991@sina.com>
2017-09-07 12:16:27 +0800

    refactor crawler.js

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,12 +1,11 @@
-const config = require('../config/config');
 const request = require('request');
-const cheerio = require('cheerio');
-const utils = require('../utils/utils');
 const chalk = require('chalk');
+const parse = require('./parse');
+const utils = require('../utils/utils');
+const config = require('../config/config');
 const saveModel = require('../model/save_model');
-const trim = utils.trimStr;
-const strictTrim = utils.strictTrim;
 
+const parseBody = parse.parseBody;
 const url = config.url;
 const stationArr = config.stationArr;
 const modelSave = saveModel.modelSave;
@@ -17,27 +16,7 @@ const execute = () => {
     encoding: 'utf-8'
   }, (err, res, body) => {
     if (res) {
-      const $ = cheerio.load(body);
-      let result = [];
-      $('.js_fang_list li').each((i, elem) => {
-        let info = {};
-        const $ = cheerio.load(elem);
-        const title = trim($('.prop-title').text());
-        const mainInfo = strictTrim($('.info-table .row1-text').text());
-        const totalPrice = trim($('.info-table .total-price').text());
-        const unitPrice = $('.info-table .price-item').eq(1).text();
-        const location = strictTrim($('.property-tag-container span').eq(0).text());
-        const specialExplain = $('.property-tag-container span').eq(1).text();
-        info = {
-          title: title,
-          mainInfo: mainInfo,
-          unitPrice: unitPrice,
-          totalPrice: totalPrice,
-          specialExplain: specialExplain
-        };
-        console.dir(info);
-        result.push(info);
-      })
+      const result = parseBody(body);
       modelSave(result);
     }
   })
---
+++ crawler/parse.js
@@ -0,0 +1,35 @@
+const cheerio = require('cheerio');
+const utils = require('../utils/utils');
+
+const matchReg = utils.matchReg;
+const trim = utils.trimStr;
+const strictTrim = utils.strictTrim;
+
+const parseBody = body => {
+  const $ = cheerio.load(body);
+  let result = [];
+  $('.js_fang_list li').each((i, elem) => {
+    let info = {};
+    const $ = cheerio.load(elem);
+    const title = trim($('.prop-title').text());
+    const mainInfo = strictTrim($('.info-table .row1-text').text());
+    const totalPrice = trim($('.info-table .total-price').text());
+    const unitPrice = matchReg(strictTrim($('.info-table .price-item').eq(1).text()), 'num');
+    const location = strictTrim($('.property-tag-container span').eq(0).text());
+    const specialExplain = $('.property-tag-container span').eq(1).text();
+    info = {
+      title: title,
+      mainInfo: mainInfo,
+      unitPrice: unitPrice,
+      totalPrice: totalPrice,
+      specialExplain: specialExplain
+    };
+    console.dir(info);
+    result.push(info);
+  })
+  return result;
+}
+
+module.exports = {
+  parseBody: parseBody
+}
